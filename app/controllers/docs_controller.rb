c7eda84bdac693c7aa7f5cb78dab36b0e253b7a7
erimerturk <erimerturk@gmail.com>
2019-03-07 17:18:55 +0300

    worked on docs display and index

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,7 +1,9 @@
 class DocsController < ApplicationController
 
-  def index
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @docs = Doc.all.order("created_at DESC")
   end
 
   def show
@@ -9,10 +11,18 @@ class DocsController < ApplicationController
   end
 
   def new
+    @doc = Doc.new
 
   end
 
   def create
+    @doc = Doc.new(doc_params)
+
+    if @doc.save
+      redirect_to @doc  #makes new request
+    else
+      render 'new'    #dont make new request just render on same page
+    end
 
   end
 
@@ -30,13 +40,13 @@ class DocsController < ApplicationController
   end
 
   private
-  
-    def find_doc
 
+    def find_doc
+      @doc = Doc.find(params[:id])
     end
 
     def doc_params
-
+      params.require(:doc).permit(:title, :content)
     end
 
 end
