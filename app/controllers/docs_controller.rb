9b95fa5caf170657725673203381a77ebc2e5bcd
erimerturk <erimerturk@gmail.com>
2019-03-07 17:32:15 +0300

    worked on create, update and show pages

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -32,10 +32,17 @@ class DocsController < ApplicationController
 
 
   def update
-
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+
+    @doc.destroy
+    redirect_to docs_path
 
   end
 
